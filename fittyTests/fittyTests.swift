ac14d590ec04a766fd89756f4b08c46d3e13e622
Seth Horsley <github.dvxhi@sl.isethi.com>
2024-10-05 22:32:42 +0200

    Initial Commit

---
+++ fittyTests/fittyTests.swift
@@ -0,0 +1,17 @@
+//
+//  fittyTests.swift
+//  fittyTests
+//
+//  Created by send on 10/5/24.
+//
+
+import Testing
+@testable import fitty
+
+struct fittyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
